6b2a2cf309c1a120a6ae09afb91a937030b1ab3d
Matiyas Berhane <mbirhane@kifiya.et>
2025-10-26 20:48:58 +0300

    Spring cloud gateway  - security config to authenticate all requests with keycloak

---
+++ apigateway/src/main/java/com/kifiya/hackhaton/apigateway/config/GatewaySecurityConfig.java
@@ -0,0 +1,32 @@
+package com.kifiya.hackhaton.apigateway.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class GatewaySecurityConfig {
+
+    @Autowired
+    private KeycloakRealmRoleConverter keycloakRealmRoleConverter;
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers(HttpMethod.OPTIONS,"/**").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults())
+                );
+        return http.build();
+    }
+
+}
